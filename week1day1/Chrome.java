fbcd3a2ad64232f5ef5667b72e91fd64e6782247
KavitaBharti8888 <kavita.bharti.sharma@gmail.com>
2025-03-08 14:50:37 +0530

    Add files via upload

---
+++ week1day1/Chrome.java
@@ -0,0 +1,24 @@
+package week1day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+		int year=2008;
+		float b=91.0f;
+		String s1="Google";
+		char c='C';
+		boolean b1=false;
+		System.out.println(year);
+		System.out.println(b);
+		System.out.println(s1);
+		System.out.println(c);
+		System.out.println(b1);
+		
+		
+		
+	}
+
+}
